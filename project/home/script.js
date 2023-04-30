1e738e90b3745526383ac8798e203f2d721bebc0
MinKhant610 <minkhantiopp@gmail.com>
2023-04-30 16:50:44 +0630

    complete home

--- project/home/script.js
+++ project/home/script.js
@@ -1,29 +1,38 @@
 let red_color = document.querySelectorAll('.red');
-let blue_color = document.querySelector('.blue');
-let yellow_color = document.querySelector('.yellow');
+let blue_color = document.querySelectorAll('.blue');
+let yellow_color = document.querySelectorAll('.yellow');
+let all_color = [red_color , blue_color , yellow_color];
 
-const first_red = red_color[0];
-const second_red = red_color[1];
-const third_red = red_color[2];
-const four_red = red_color[3];
-
-const home = document.querySelector('.home');
 const roof = document.querySelector('.roof');
 const home_back = document.querySelector('.home-back');
 const home_window = document.querySelector('.home-window');
 const home_door = document.querySelector('.home-door');
 
-second_red.addEventListener('click',  ()=> {
-    // let home_first = home.children[1].className;
-    console.log('hello');
-    
-})
+for(let i = 0; i < 4; i++){
+    findWhichEle(red_color , i );
+    findWhichEle(yellow_color , i);
+    findWhichEle(blue_color ,i );
+}
+function findWhichEle(colorAry, i){
+    let color_element = colorAry[i];
+    color_element.onclick = () =>{
+        switch(i){
+            case 0 :
+                changeColor(roof , color_element.classList);
+                break;
+            case 1 :
+                changeColor(home_back , color_element.classList);
+                break;
+            case 2 :
+                changeColor(home_window , color_element.classList);
+                break;
+            case 3 :
+                changeColor(home_door , color_element.classList);
+                break;
+        }
+    }
+}
 
-
-blue_color.addEventListener('click',  ()=> {
-    roof.style.backgroundColor = 'blue';
-})
-
-yellow_color.addEventListener('click',  ()=> {
-    roof.style.backgroundColor = 'yellow';
-})
+function changeColor(element , color ){
+    element.style.backgroundColor = color;
+}
